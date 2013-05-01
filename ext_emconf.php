b5497e1318c8c65d8c607198b4d40124b853660f
Arno Dudek <arno.dudek@maas.at>
2013-05-01 15:55:14 +0200

    [TASK] Tagged version 1.0.1

--- ext_emconf.php
+++ ext_emconf.php
@@ -3,7 +3,7 @@
 /***************************************************************
  * Extension Manager/Repository config file for ext "ad_google_maps_plugin_kml".
  *
- * Auto generated 03-04-2013 10:25
+ * Auto generated 01-05-2013 15:34
  *
  * Manual updates:
  * Only the data in the array - everything else is removed by next
@@ -15,13 +15,13 @@ $EM_CONF[$_EXTKEY] = array(
 	'description' => 'Extends the extension ad: Google Maps (ad_google_maps) with a KML layer.',
 	'category' => 'plugin',
 	'shy' => 0,
-	'version' => '1.0.0',
+	'version' => '1.0.1',
 	'dependencies' => '',
 	'conflicts' => '',
 	'priority' => '',
 	'loadOrder' => '',
 	'module' => '',
-	'state' => 'beta',
+	'state' => 'stable',
 	'uploadfolder' => 0,
 	'createDirs' => '',
 	'modify_tables' => '',
@@ -44,7 +44,7 @@ $EM_CONF[$_EXTKEY] = array(
 		'suggests' => array(
 		),
 	),
-	'_md5_values_when_last_written' => 'a:17:{s:9:"ChangeLog";s:4:"c76b";s:21:"ext_conf_template.txt";s:4:"43a1";s:12:"ext_icon.gif";s:4:"f587";s:14:"ext_tables.php";s:4:"36ef";s:14:"ext_tables.sql";s:4:"9785";s:25:"Classes/Api/Layer/Kml.php";s:4:"a289";s:34:"Classes/Domain/Model/Layer/Kml.php";s:4:"f950";s:32:"Classes/MapBuilder/Layer/Kml.php";s:4:"1f0d";s:36:"Classes/Plugin/Options/Layer/Kml.php";s:4:"c6b3";s:34:"Configuration/TypoScript/setup.txt";s:4:"8562";s:48:"Resources/Private/Language/locallang_extconf.xml";s:4:"15ce";s:44:"Resources/Private/Language/locallang_tca.xml";s:4:"5dd5";s:54:"Resources/Private/Language/locallang_tca_csh_layer.xml";s:4:"09be";s:38:"Resources/Public/Icons/TCA/IconKml.gif";s:4:"4460";s:73:"Resources/Public/JavaScript/Plugin/Tx_AdGoogleMapsPluginKml_Plugin_Kml.js";s:4:"cad0";s:32:"Resources/Public/KmlTest/cta.kml";s:4:"62b2";s:14:"doc/manual.sxw";s:4:"cb82";}',
+	'_md5_values_when_last_written' => 'a:17:{s:9:"ChangeLog";s:4:"5a1f";s:21:"ext_conf_template.txt";s:4:"43a1";s:12:"ext_icon.gif";s:4:"f587";s:14:"ext_tables.php";s:4:"36ef";s:14:"ext_tables.sql";s:4:"9785";s:25:"Classes/Api/Layer/Kml.php";s:4:"a289";s:34:"Classes/Domain/Model/Layer/Kml.php";s:4:"76d9";s:32:"Classes/MapBuilder/Layer/Kml.php";s:4:"c7f8";s:36:"Classes/Plugin/Options/Layer/Kml.php";s:4:"c6b3";s:34:"Configuration/TypoScript/setup.txt";s:4:"8562";s:48:"Resources/Private/Language/locallang_extconf.xml";s:4:"15ce";s:44:"Resources/Private/Language/locallang_tca.xml";s:4:"5dd5";s:54:"Resources/Private/Language/locallang_tca_csh_layer.xml";s:4:"09be";s:38:"Resources/Public/Icons/TCA/IconKml.gif";s:4:"4460";s:73:"Resources/Public/JavaScript/Plugin/Tx_AdGoogleMapsPluginKml_Plugin_Kml.js";s:4:"cad0";s:32:"Resources/Public/KmlTest/cta.kml";s:4:"62b2";s:14:"doc/manual.sxw";s:4:"cb82";}',
 );
 
 ?>
